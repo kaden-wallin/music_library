9848530cf523424d97882eb259a8b0c40eb803a4
BuildTools <unconfigured@null.spigotmc.org>
2023-01-05 09:53:19 -0700

    Added styling and the argument to GalleryItems function

--- src/Components/GalleryItem.jsx
+++ src/Components/GalleryItem.jsx
@@ -1,10 +1,29 @@
 import { useState } from "react"
 
-function GalleryItem() {
+function GalleryItem(props) {
     const [viewDetails, setViewDetails] = useState(false)
+    
+    const simpleStyle = {
+        'width': '25vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px'
+    }
+
+    const detailStyle = {
+        'width': '80vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px',
+        'backgroundImage': `url(${props.item.artworkUrl100})`,
+        'backgroundRepeat': 'no-repeat',
+        'backgroundSize': 'cover',
+        'color': 'yellow'
+    }
+    
     const simpleView = () => {
         return (
-            <div>
+            <div style={{simpleStyle}}>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
             </div>
@@ -13,7 +32,7 @@ function GalleryItem() {
 
     const detailsView = () => {
         return (
-            <div>
+            <div style={{detailStyle}}>
                 <h2>{props.item.trackName}</h2>
                 <h3>{props.item.collectionName}</h3>
                 <h4>{props.item.primaryGenreName}</h4>
