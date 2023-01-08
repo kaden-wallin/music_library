1487268d9f61d657ac1e381ee9f9c11164cc903f
BuildTools <unconfigured@null.spigotmc.org>
2023-01-08 15:01:44 -0700

    Added some routes and set up the bare-bones album/artist pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Gallery from './Components/Gallery';
 import SearchBar from './Components/SearchBar';
+import AlbumView from './Components/AlbumView';
+import ArtistView from './Components/ArtistView';
 import { DataContext } from './Context/DataContext';
-import { SearchContext, SearchCOntext } from './Context/SearchContext'
+import { SearchContext } from './Context/SearchContext'
 
 function App() {
   const [data, setData] = useState([])
@@ -30,16 +33,26 @@ function App() {
 
   return (
     <div className="App">
-      <SearchContext.Provider value={{
-        term: searchInput,
-        handleSearch: handleSearch
-      }}>
-        <SearchBar handleSearch={handleSearch} />
-      </SearchContext.Provider>
       {message}
-      <DataContext.Provider value={data} >
-        <Gallery />
-      </DataContext.Provider>
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <Fragment>
+              <SearchContext.Provider value={{
+              term: searchInput,
+              handleSearch: handleSearch
+              }}>
+              <SearchBar handleSearch={handleSearch} />
+              </SearchContext.Provider>
+              <DataContext.Provider value={data} >
+              <Gallery />
+              </DataContext.Provider>
+            </Fragment>
+          } />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
